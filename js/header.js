af8d19f0cc4e6bd2d83db14873487d61da795c1a
jakubjirimarek <jakubjirimarek@gmail.com>
2025-12-16 20:24:15 +0100

    edit

--- js/header.js
+++ js/header.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector("header");
   let lastTouchY = null;
 
+  const isDesktop = () => window.innerWidth > 990;
+
   window.addEventListener(
     "wheel",
     (e) => {
+      if (!isDesktop()) return;
+
       if (e.deltaY > 0) header.classList.add("hidden");
       if (e.deltaY < 0) header.classList.remove("hidden");
     },
@@ -14,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener(
     "touchstart",
     (e) => {
+      if (!isDesktop()) return;
       lastTouchY = e.touches[0].clientY;
     },
     { passive: true }
@@ -22,7 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener(
     "touchmove",
     (e) => {
+      if (!isDesktop()) return;
       if (lastTouchY === null) return;
+
       const y = e.touches[0].clientY;
 
       if (y < lastTouchY) header.classList.add("hidden");
