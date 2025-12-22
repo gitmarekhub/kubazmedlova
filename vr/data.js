914388b04cde5841529f5daf7df304057e234f04
jakubjirimarek <jakubjirimarek@gmail.com>
2025-12-22 13:11:23 +0100

    edit

--- vr/data.js
+++ vr/data.js
@@ -24,22 +24,16 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": 0.5043674623759706,
-        "pitch": 0.1668133816668096,
+        "yaw": 0.39508710921897716,
+        "pitch": 0.005232712858298072,
         "fov": 1.4010588678110822
       },
       "linkHotspots": [
         {
-          "yaw": 0.43224909189659044,
-          "pitch": 0.6194998175905706,
+          "yaw": 0.4711446187165187,
+          "pitch": 0.6260024927190138,
           "rotation": 0,
           "target": "1-chodba"
-        },
-        {
-          "yaw": 0.2629430014271925,
-          "pitch": 0.3190381027078182,
-          "rotation": 0,
-          "target": "2-chodba---zrcadlo"
         }
       ],
       "infoHotspots": []
@@ -74,41 +68,41 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.474733240228396,
-          "pitch": 0.6660310477101223,
+          "yaw": -2.4004581185803584,
+          "pitch": 0.6722558682369488,
           "rotation": 0,
           "target": "0-vstup"
         },
         {
-          "yaw": -0.010583481648298587,
-          "pitch": 0.43804285838428214,
+          "yaw": -0.023689358223387913,
+          "pitch": 0.5042362458396568,
           "rotation": 0,
-          "target": "2-chodba---zrcadlo"
+          "target": "2-zrcadlo"
         },
         {
-          "yaw": 2.8012440575934923,
-          "pitch": 0.6862129999285127,
+          "yaw": 2.798672098145257,
+          "pitch": 0.6835004642744487,
           "rotation": 0,
           "target": "3-koupelna"
         },
         {
-          "yaw": 1.4943995799301728,
-          "pitch": 0.5695400062197997,
+          "yaw": 1.5128502099299332,
+          "pitch": 0.574158086659768,
           "rotation": 0,
           "target": "5-lonice---vstup"
         },
         {
-          "yaw": 0.566271097293713,
-          "pitch": 0.5228720785527372,
+          "yaw": 0.5796575725133906,
+          "pitch": 0.5984507687062166,
           "rotation": 0,
-          "target": "7-dtsk-pokoj"
+          "target": "7-dtsk-pokoj---vstup"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-chodba---zrcadlo",
-      "name": "Chodba - zrcadlo",
+      "id": "2-zrcadlo",
+      "name": "Zrcadlo",
       "levels": [
         {
           "tileSize": 256,
@@ -136,28 +130,28 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -3.0641144647741925,
-          "pitch": 0.654898158443789,
-          "rotation": 0,
-          "target": "1-chodba"
-        },
-        {
-          "yaw": -1.0312845594163562,
-          "pitch": 0.7191665107939293,
+          "yaw": -1.0339842288797705,
+          "pitch": 0.7588074170127737,
           "rotation": 0,
           "target": "4-wc"
         },
         {
-          "yaw": 2.125678013196783,
-          "pitch": 0.7040697676258247,
+          "yaw": -2.985309176273333,
+          "pitch": 0.7088997597487463,
           "rotation": 0,
-          "target": "7-dtsk-pokoj"
+          "target": "1-chodba"
         },
         {
-          "yaw": -0.1657625397762068,
-          "pitch": 0.35689131367658966,
+          "yaw": -0.18116068991638912,
+          "pitch": 0.3705990947228237,
           "rotation": 0,
           "target": "9-kuchyn"
+        },
+        {
+          "yaw": 2.1028753521178682,
+          "pitch": 0.6831719171612054,
+          "rotation": 0,
+          "target": "7-dtsk-pokoj---vstup"
         }
       ],
       "infoHotspots": []
@@ -186,22 +180,16 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.34776574577825414,
+        "pitch": 0.10910229227913604,
+        "fov": 1.4010588678110822
       },
       "linkHotspots": [
         {
-          "yaw": 3.0150284968854493,
-          "pitch": 0.7063629860919463,
+          "yaw": 2.9954394401139215,
+          "pitch": 0.6634219602555564,
           "rotation": 0,
           "target": "1-chodba"
-        },
-        {
-          "yaw": 3.069430927847904,
-          "pitch": 0.33411368163709554,
-          "rotation": 0,
-          "target": "2-chodba---zrcadlo"
         }
       ],
       "infoHotspots": []
@@ -230,23 +218,23 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.15791223769245732,
+        "pitch": 0.5108457531548822,
+        "fov": 1.4010588678110822
       },
       "linkHotspots": [
         {
-          "yaw": 1.543002728136801,
-          "pitch": 0.818280795521293,
+          "yaw": 1.6928011774877119,
+          "pitch": 0.8297149824151404,
           "rotation": 0,
-          "target": "2-chodba---zrcadlo"
+          "target": "2-zrcadlo"
         }
       ],
       "infoHotspots": []
     },
     {
       "id": "5-lonice---vstup",
-      "name": "Ložnice - vstup",
+      "name": "Ložnice - Vstup",
       "levels": [
         {
           "tileSize": 256,
@@ -274,23 +262,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.113083872284788,
-          "pitch": 0.4100496359358914,
-          "rotation": 0,
-          "target": "6-lonice---sted"
-        },
-        {
-          "yaw": -3.073548314578847,
-          "pitch": 0.7268825608741665,
+          "yaw": -2.99262633177597,
+          "pitch": 0.7161025967579508,
           "rotation": 0,
           "target": "1-chodba"
+        },
+        {
+          "yaw": 0.12150898231311658,
+          "pitch": 0.4294997510230303,
+          "rotation": 0,
+          "target": "6-lonice---sted"
         }
       ],
       "infoHotspots": []
     },
     {
       "id": "6-lonice---sted",
-      "name": "Ložnice - střed",
+      "name": "Ložnice - Střed",
       "levels": [
         {
           "tileSize": 256,
@@ -318,8 +306,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.6832858902443864,
-          "pitch": 0.5956959749818029,
+          "yaw": 1.6828686870685585,
+          "pitch": 0.510260302662104,
           "rotation": 0,
           "target": "5-lonice---vstup"
         }
@@ -327,8 +315,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "7-dtsk-pokoj",
-      "name": "Dětský pokoj",
+      "id": "7-dtsk-pokoj---vstup",
+      "name": "Dětský pokoj - Vstup",
       "levels": [
         {
           "tileSize": 256,
@@ -356,14 +344,14 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.593763590786846,
-          "pitch": 0.6162988566979237,
+          "yaw": 2.6624157262235446,
+          "pitch": 0.6883043364369108,
           "rotation": 0,
           "target": "1-chodba"
         },
         {
-          "yaw": -0.31560179758708173,
-          "pitch": 0.5818951476338405,
+          "yaw": -0.41089978375692127,
+          "pitch": 0.47693151006668444,
           "rotation": 0,
           "target": "8-dtsk-pokoj---sted"
         }
@@ -372,7 +360,7 @@ var APP_DATA = {
     },
     {
       "id": "8-dtsk-pokoj---sted",
-      "name": "Dětský pokoj - střed",
+      "name": "Dětský pokoj - Střed",
       "levels": [
         {
           "tileSize": 256,
@@ -394,16 +382,16 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.4521948614710105,
+        "pitch": -0.011773603931196419,
+        "fov": 1.4010588678110822
       },
       "linkHotspots": [
         {
-          "yaw": 2.8448076147244583,
-          "pitch": 0.5053727006657986,
+          "yaw": 2.821012599071997,
+          "pitch": 0.537536507306033,
           "rotation": 0,
-          "target": "7-dtsk-pokoj"
+          "target": "7-dtsk-pokoj---vstup"
         }
       ],
       "infoHotspots": []
@@ -438,16 +426,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.7483490232274086,
-          "pitch": 0.6478768679387841,
-          "rotation": 0,
-          "target": "2-chodba---zrcadlo"
-        },
-        {
-          "yaw": 1.1595069845692603,
-          "pitch": 0.3659908925822517,
+          "yaw": 1.1209676645628903,
+          "pitch": 0.37505548915994424,
           "rotation": 0,
           "target": "10-obvk"
+        },
+        {
+          "yaw": -2.9131466038012857,
+          "pitch": 0.6951194482013605,
+          "rotation": 0,
+          "target": "2-zrcadlo"
         }
       ],
       "infoHotspots": []
@@ -482,8 +470,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.7669648670045683,
-          "pitch": 0.43017417648472467,
+          "yaw": 2.8342232576603177,
+          "pitch": 0.3823554257155486,
           "rotation": 0,
           "target": "9-kuchyn"
         }
@@ -491,7 +479,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Byt_3KK",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
